8cfc5b1f847e43ff604b6b054e01bbc9e0742c7f
Ian McKenzie <ianmck98@gmail.com>
2025-07-09 16:18:08 -0700

    add project filtering dropdown

--- mlflow/server/js/src/experiment-tracking/components/ExperimentListPage.tsx
+++ mlflow/server/js/src/experiment-tracking/components/ExperimentListPage.tsx
@@ -1,10 +1,11 @@
 import ExperimentListView from './ExperimentListView';
-import { useSearchFilter } from './experiment-page/hooks/useSearchFilter';
+import { useSearchFilter, useProjectFilter } from './experiment-page/hooks/useSearchFilter';
 
 const ExperimentListPage = () => {
   const [searchFilter, setSearchFilter] = useSearchFilter();
+  const [projectFilter, setProjectFilter] = useProjectFilter();
 
-  return <ExperimentListView searchFilter={searchFilter} setSearchFilter={setSearchFilter} />;
+  return <ExperimentListView searchFilter={searchFilter} setSearchFilter={setSearchFilter} projectFilter={projectFilter} setProjectFilter={setProjectFilter} />;
 };
 
 export default ExperimentListPage;
